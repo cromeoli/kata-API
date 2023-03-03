60e1348db8ba560cd98e82fcbfa20d5d7ee34518
cromeoli <cromero@iesrafaelalberti.es>
2023-03-03 04:36:41 +0100

    prueba del setup

---
+++ src/main/java/syntaxt3rr0r/apikata/ApiKataApplication.java
@@ -0,0 +1,13 @@
+package syntaxt3rr0r.apikata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/syntaxt3rr0r/apikata/ApiKataApplicationTests.java
@@ -0,0 +1,13 @@
+package syntaxt3rr0r.apikata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
