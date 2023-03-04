2a0a1fd89ef1fe1171e25e08a97d01d1f8cc9bcc
jlorlop980 <jalorenzo@iesrafaelalberti.es>
2023-03-04 19:15:59 +0100

    Category y categories GET

---
+++ src/main/java/syntaxt3rr0r/apikata/dto/CategoriesDTO.java
@@ -0,0 +1,12 @@
+package syntaxt3rr0r.apikata.dto;
+
+import syntaxt3rr0r.apikata.modelo.Categories;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CategoriesDTO {
+
+    private long id;
+    private String cat_name;
+}
