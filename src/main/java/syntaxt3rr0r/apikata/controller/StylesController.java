4d033757403862dda7ecbe4f936592472ca8fe43
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2023-03-04 18:37:14 +0100

    get all styles (id funciona, cat name y name son null)

---
+++ src/main/java/syntaxt3rr0r/apikata/controller/StylesController.java
@@ -0,0 +1,62 @@
+package syntaxt3rr0r.apikata.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import syntaxt3rr0r.apikata.dto.BeersDTO;
+import syntaxt3rr0r.apikata.dto.CreateBeersDTO;
+import syntaxt3rr0r.apikata.dto.StylesDTO;
+import syntaxt3rr0r.apikata.dto.converter.BeersDTOConverter;
+import syntaxt3rr0r.apikata.dto.converter.StylesDTOConverter;
+import syntaxt3rr0r.apikata.modelo.Beers;
+import syntaxt3rr0r.apikata.modelo.Categories;
+import syntaxt3rr0r.apikata.modelo.Styles;
+import syntaxt3rr0r.apikata.repo.BeersRepo;
+import syntaxt3rr0r.apikata.repo.CategoriesRepo;
+import syntaxt3rr0r.apikata.repo.StylesRepo;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class StylesController {
+
+    private final StylesDTOConverter stylesDTOConverter;
+    private final StylesRepo stylesRepo;
+
+
+    /**
+     * Obtenemos todos los estilos
+     * @return 404 si no hay estilos, 200 y lista de estilos si hay uno o más
+     */
+    @GetMapping("/styles")
+    public ResponseEntity<?> getAllStyles() {
+        List <Styles> result = stylesRepo.findAll();
+
+        if(result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            List<StylesDTO> dtoList = result.stream().map(stylesDTOConverter::convertToDto)
+                    .collect(Collectors.toList());
+
+            return ResponseEntity.ok(dtoList);
+        }
+    }
+
+    /**
+     * Obtengo un estilo por su id
+     * @param id
+     * @return 404 si no encuentra el estilo, si lo encuentra devuelve 200 y el estilo
+     */
+    @GetMapping("/styles/{id}")
+    public ResponseEntity<?> getOneStyle(@PathVariable Long id) {
+        Styles result = stylesRepo.findById(id).orElse(null);
+        return (result == null) ?
+                ResponseEntity.notFound().build() :
+                ResponseEntity.ok(result);
+    }
+}
---
+++ src/main/java/syntaxt3rr0r/apikata/dto/StylesDTO.java
@@ -0,0 +1,20 @@
+package syntaxt3rr0r.apikata.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import syntaxt3rr0r.apikata.modelo.Categories;
+
+/**
+ * La tabla styles ofrece id, id de la categoría, nombre del estilo, y fecha de última modificación.
+ * Al devolver el listado, vamos a devolver sólo la id, el nombre del estilo y el nombre de la categoría.
+ * Para esto vamos a usar un DTO.
+ */
+
+@Getter @Setter
+public class StylesDTO {
+    private long id;
+    private String name;
+    private String categoriesCat_name;
+
+}
